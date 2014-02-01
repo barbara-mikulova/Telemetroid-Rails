09ca0054521ecf786f80b3b650e673d543e7ece4
Rashitko <michal.raska@gmail.com>
2014-02-01 17:30:04 +0100

    Vytvorena metoda na pridanie do jedneho feedu a jej routa

--- app/controllers/shared_infos_controller.rb
+++ app/controllers/shared_infos_controller.rb
@@ -1,8 +1,14 @@
 class SharedInfosController < ApplicationController
 
+  protect_from_forgery :except => [:add_one]
+
   def full_index
     infos = SharedInfo.all
     render json: infos
+  end
+
+  def add_one
+    render text: 'add data to one feed'
   end
 
 end
