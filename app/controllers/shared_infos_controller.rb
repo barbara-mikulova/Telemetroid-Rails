4f3d6662703b41f7537b20fc042655c3b2e71695
Rashitko <michal.raska@gmail.com>
2014-02-01 17:45:21 +0100

    Implementovane uploadovanie udajov do jedneho feedu

--- app/controllers/shared_infos_controller.rb
+++ app/controllers/shared_infos_controller.rb
@@ -1,6 +1,8 @@
 class SharedInfosController < ApplicationController
 
-  protect_from_forgery :except => [:add_one]
+  protect_from_forgery except: [:add_one]
+
+  before_action :require_feed_existence, :require_device_existence, except: [:full_index]
 
   def full_index
     infos = SharedInfo.all
@@ -8,7 +10,37 @@ class SharedInfosController < ApplicationController
   end
 
   def add_one
-    render text: 'add data to one feed'
+    create_shared_info
+    if (@info.save)
+      render json: @info
+    else
+      render_save_errors(@info)
+      return
+    end
+  end
+
+  def create_shared_info
+    @info = SharedInfo.create
+    @info.device = @device
+    @info.feed = @feed
+    @info.json = params[:json]
+  end
+
+  private
+  def require_device_existence
+    @device = Device.find_by_identifier(params[:device_identif])
+    if not @device
+      error_missing_entry(["Can't find device"])
+      return
+    end
+  end
+
+  def require_feed_existence
+    @feed = Feed.find_by_identifier(params[:identifier])
+    if not @feed
+      error_missing_entry(["Can't find feed"])
+      return
+    end
   end
 
 end
--- app/models/shared_info.rb
+++ app/models/shared_info.rb
@@ -1,4 +1,9 @@
 class SharedInfo < ActiveRecord::Base
   belongs_to :feed
   belongs_to :device
+
+  validates :feed_id, presence: true
+  validates :device_id, presence: true
+  validates :json, presence: true
+
 end
