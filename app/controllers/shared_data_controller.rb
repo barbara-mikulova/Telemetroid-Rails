62372a2ac72d2ab20881d72139d7aada4cd50988
Rashitko <michal.raska@gmail.com>
2014-02-22 16:38:49 +0100

    Implementovane pristupove prava pre write do kanalu, zatial iba pre uzivatela.

--- app/controllers/shared_data_controller.rb
+++ app/controllers/shared_data_controller.rb
@@ -2,52 +2,85 @@ class SharedDataController < ApplicationController
 
   protect_from_forgery :except => [:full_index, :insert]
 
+  before_action :require_login
+
+  before_action :get_writable_feeds, :only =>[:insert]
+
   def full_index
     data = SharedData.all
     render json: data
   end
 
   def insert
-    feed_ids = params[:feedsIdentifiers]
-    type = params[:trackType]
-    device_id = params[:deviceIdentifier]
     entries = params[:entries]
-    device = Device.find_by_identifier(device_id)
-    track_id = get_track_id(device, type)
-    feeds = get_feeds_from_ids(feed_ids)
+    track_id = get_track_id
+    if @errors.length > 0
+      if @feeds.length > 0
+        text = 'Write successful only for feeds: '
+        @feeds.each do |feed|
+          text += feed.name + ', '
+        end
+        text = text[0..-3]
+        @errors += [text]
+      end
+    end
     entries.each do |entry|
       new_data = SharedData.new
       new_data.json_data = entry['data']
       new_data.time_stamp = entry['timeStamp']
-      new_data.device = device
+      new_data.device = @device
       new_data.track_id = track_id
-      new_data.feeds += feeds
+      new_data.feeds += @feeds
       if new_data.save
-        device.save
+        @device.save
       end
     end
-    response_ok
+    if @errors.length > 0
+      error_denied(@errors)
+    else
+      response_ok
+    end
+  end
+
+  def get_writable_feeds
+    feed_ids = params[:feedsIdentifiers]
+    @feeds = Array.new
+    @errors = Array.new
+    @device = Device.find_by_identifier(params[:deviceIdentifier])
+    unless @device
+      cant_find_device
+      return
+    end
+    feed_ids.each do |feed_id|
+      feed = Feed.find_by_identifier(feed_id)
+      unless feed
+        error_missing_entry(["Can't find feed with identifier: " + feed_id])
+        return
+      end
+      if (session[:type] == 'user') and (feed.writers.find_by_user_id(@device.user.id) != nil)
+        @feeds.push(feed)
+      else
+        @errors += ["Can't write to " + feed.name + '. Ask administrator for permission.']
+      end
+    end
   end
 
   private
-  def get_feeds_from_ids(feed_ids)
-    feeds = Array.new
-    feed_ids.each do |feed_id|
-      feed = Feed.find_by_identifier(feed_id)
-      feeds.push(feed)
-    end
-    return feeds
+  def cant_find_device
+    error_missing_entry(["Can't find device with identifier: " + params[:deviceIdentifier]])
+
   end
 
-  def get_track_id(device, type)
+  def get_track_id
+    type = params[:trackType]
     if type == 'standalone'
       return -1
     end
     if type == 'new'
-      device.current_track += 1;
-      return device.current_track
+      @device.current_track += 1
+      return @device.current_track
     end
-    return device.current_track
+    return @device.current_track
   end
-
 end
+
