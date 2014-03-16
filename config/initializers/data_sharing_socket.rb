5f000924f9f9c8f0d1cd56ee179ba9ec8aa3b1e3
Michal Raška <michal.raska@gmail.com>
2014-03-16 14:08:46 +0100

    Pridany login usera do socketu. Meno a heslo sa zatial posielaju v URL. Pridana kontrola na prava do kanalu. Pri write connecte sa kanaly posielaju ako URL parametre oddelene ciarkami, pri reade priamo v URL adrese.

--- config/initializers/data_sharing_socket.rb
+++ config/initializers/data_sharing_socket.rb
@@ -4,10 +4,13 @@ Thread.abort_on_exception = true
 
 Thread.new {
   EventMachine.run {
-    @sockets = Array.new
+    @logged_users = Array.new
+    @logged_user_map = {}
+    @channelMap = {}
+
     EventMachine::WebSocket.start(:host => '0.0.0.0', :port => '8080') do |ws|
-      ws.onopen do
-        @sockets.push(ws)
+      ws.onopen do |handshake|
+        login_user(handshake, ws)
       end
 
       ws.onclose do
@@ -15,11 +18,57 @@ Thread.new {
       end
 
       ws.onmessage do |msg|
-        puts msg
-        @sockets.each do |socket|
-          socket.send(msg)
-        end
+        json = JSON.parse(msg)
+        puts json
       end
     end
   }
 }
+
+private
+
+def login_user(handshake, ws)
+  uri = URI(handshake.path)
+  query = handshake.query
+  puts query
+  path = uri.path.split('/')
+  puts path.size
+  if path[1] == 'user'
+    user = User.find_by_username(query['username'])
+    unless user && user.password == query['password']
+      ws.send 'Wrong username or password'
+      ws.close
+      return
+    end
+    if path[2] == 'read'
+      feed = Feed.find_by_identifier(path[3])
+      unless feed
+        ws.send "Feed with id '#{path[3]}' does not exist"
+        ws.close
+        return
+      end
+      unless feed.readers.find_by_user_id(user.id)
+        ws.send "Feed #{feed.name} can't be read"
+        ws.close
+        return
+      end
+    elsif path[2] == 'write'
+      feed_ids = query['feed_ids'].split(',')
+      feed_ids.each do |feed_id|
+        feed = Feed.find_by_identifier(feed_id)
+        unless feed
+          ws.send "Feed with id '#{feed_id}' does not exist"
+          return
+        end
+        unless feed.writers.find_by_user_id(user.id)
+          ws.send "#{feed.name} can't be written"
+          return
+        end
+      end
+      puts feed_ids
+    end
+    puts 'login user'
+  elsif path[1] == 'device'
+    puts 'login device'
+  end
+end
