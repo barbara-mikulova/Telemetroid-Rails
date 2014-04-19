4867c3853087e5a20b16a42e7269189da7c826ce
Michal Raška <michal.raska@gmail.com>
2014-04-19 14:37:25 +0200

    Odstranene spravy.

--- app/models/message.rb
+++
@@ -1,8 +0,0 @@
-class Message < ActiveRecord::Base
-  belongs_to :device
-  belongs_to :user
-
-  validates :message, presence: true
-  validates :user_id, presence: true
-  validates :device_id, presence: true
-end
